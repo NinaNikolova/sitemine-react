e10669ad51f6e68736337c06e6e14a3bc826d249
nina <ninagbs@abv.bg>
2023-09-16 18:25:50 +0300

    added backend folder with express and cors

---
+++ backend/src/data.js
@@ -0,0 +1,79 @@
+export const sample_sites = [
+    {
+        id: '1',
+        name: 'Магазин',
+        prepareTime: '5',
+        price: '6',
+        favorite: false,
+        origins: ['bg'],
+        stars: 4.5,
+        imageUrl: '/sites/site-1.jpg',
+        tags: ['Магазин', 'Информация']
+    },
+    {
+        id: '2',
+        name: 'Визитка',
+        prepareTime: '3',
+        price: '4',
+        favorite: false,
+        origins: ['bg', 'eng', 'gr'],
+        stars: 4,
+        imageUrl: '/sites/site-2.jpg',
+        tags: ['Информация', 'Визитка']
+    },
+    {
+        id: '3',
+        name: 'CV',
+        prepareTime: '2',
+        price: '5',
+        favorite: false,
+        origins: ['eng'],
+        stars: 3,
+        imageUrl: '/sites/site-3.jpg',
+        tags: ['Резюме', 'CV']
+    },
+    {
+        id: '4',
+        name: 'Резюме',
+        prepareTime: '2',
+        price: '5',
+        favorite: false,
+        origins: ['eng'],
+        stars: 3,
+        imageUrl: '/sites/site-4.jpg',
+        tags: ['Резюме', 'CV']
+    },
+    {
+        id: '5',
+        name: 'Блог',
+        prepareTime: '2',
+        price: '5',
+        favorite: false,
+        origins: ['bg', 'eng'],
+        stars: 3,
+        imageUrl: '/sites/site-5.jpg',
+        tags: ['Природа']
+    },
+    {
+        id: '6',
+        name: 'Рецепти',
+        prepareTime: '3',
+        price: '5',
+        favorite: false,
+        origins: ['bg', 'eng'],
+        stars: 3,
+        imageUrl: '/sites/site-6.jpg',
+        tags: ['Рецепти']
+    },
+];
+
+export const sample_tags = [
+    { name: 'Всички', count: 6 },
+    { name: 'CV', count: 2 },
+    { name: 'Резюме', count: 2 },
+    { name: 'Природа', count: 1 },
+    { name: 'Рецепти', count: 1 },
+    { name: 'Магазин', count: 1 },
+    { name: 'Информация', count: 1 },
+    { name: 'Визитка', count: 1 },
+];
