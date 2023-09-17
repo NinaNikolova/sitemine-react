7e6521bc49d88d6a461d8dc8d0677c9ba815839d
nina <ninagbs@abv.bg>
2023-09-17 17:27:37 +0300

    added login on server

--- backend/src/routers/user.router.js
+++ backend/src/routers/user.router.js
@@ -1,8 +1,36 @@
 import { Router } from "express";
+import { sample_users } from "../data.js";
+import jwt from "jsonwebtoken";
+import { BAD_REQUEST } from "../constants/httpStatus.js";
 
 const router = Router();
 // we use post usually when we want to send data to the server
 router.post('/login', (req, res) => {
     const { email, password } = req.body;
+    const user = sample_users.find(user => user.email === email && user.password === password);
+    if (user) {
+        res.send(generateTokenResponse(user));
+        return;
+    }
+    res.status(BAD_REQUEST).send('Username or password is invalid')
 
 })
+const generateTokenResponse = user => {
+    // the sign function is responsible for creating the token
+    const token = jwt.sign({
+        id: user.id,
+        email: user.email,
+        isAdmin: user.isAdmin,
+    }, 'SomeRandomText', {
+        expiresIn: '30d'
+    })
+    return {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        address: user.address,
+        isAdmin: user.isAdmin,
+        tokon,
+    }
+}
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import cors from 'cors';
-import siteRouter from './routers/site.router.js'
+import siteRouter from './routers/site.router.js';
+import userRouter from "./routers/user.router.js";
 
 const app = express();
-
+app.use(express.json());
 app.use(cors({
     credentials: true,
     origin: ['http://localhost:3000'],
 }));
 
 app.use('/api/sites', siteRouter);
+app.use('/api/users', userRouter);
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log('Listening on port' + PORT);
